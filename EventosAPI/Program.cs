8b94ca0d4cf8561ee3df27162e3edc24dc843da2
Fernando Barros <fernandes.nando@outlook.com>
2026-03-01 17:32:40 -0300

    Removendo condicional de funcionamento do Swagger em outros ambientes.

--- EventosAPI/Program.cs
+++ EventosAPI/Program.cs
@@ -63,11 +63,8 @@ namespace EventosAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
